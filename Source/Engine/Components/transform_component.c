3804cbb86982427a9ab87cd99e1847668e3ade05
Levi <johnstonlevi@hotmail.com>
2025-10-17 22:28:59 -0400

    Apply .gitignore and remove tracked build files

--- Source/Engine/Components/transform_component.c
+++
@@ -1,6 +0,0 @@
-#include "transform_component.h"
-
-TransformComponent transformComponent = {
-    .entityID = {0},                  // zero all entity IDs
-    .position = {{0,0}}               // zero all vec2_i structs
-};
